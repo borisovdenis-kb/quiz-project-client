ca5a033540d860d3ab7f4314b32fbdb746781c19
Denis Borisov <borisovdenis-kb@yandex.ru>
2018-05-06 16:39:50 +0400

    Fixed header style, added component Logo.

---
+++ src/Logo.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="logo">
+    <div>{{ decoratedText }}</div>
+    <div class="z-letter-container">
+      <div class="z-letter z0">{{ lastTextSymbol }}</div>
+      <div class="z-letter z1">{{ lastTextSymbol }}</div>
+      <div class="z-letter z2">{{ lastTextSymbol }}</div>
+      <div class="z-letter z3">{{ lastTextSymbol }}</div>
+      <div class="z-letter z4">{{ lastTextSymbol }}</div>
+      <div class="z-letter z5">{{ lastTextSymbol }}</div>
+      <div class="z-letter z6">{{ lastTextSymbol }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'logo',
+    props: ['text'],
+    data() {
+      return {
+        textLength: this.text.length
+      }
+    },
+    computed: {
+      lastTextSymbol: function () {
+        return this.text[this.textLength - 1];
+      },
+      decoratedText: function () {
+        return this.text.slice(0, this.textLength - 1).split('').join(' ');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #logo {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    color: #ffffff;
+    cursor: pointer;
+    overflow: hidden;
+  }
+
+  .z-letter-container {
+    transition: 1s ease transform;
+    transform: translateY(0);
+  }
+
+  #logo:hover .z-letter-container {
+    transform: translateY(38px);
+  }
+
+  .z-letter {
+    margin-left: 5px;
+  }
+
+  .z0 {
+    opacity: 0.6;
+  }
+
+  .z1 {
+    opacity: 1;
+  }
+
+  .z2 {
+    opacity: 0.6;
+  }
+
+  .z3 {
+    opacity: 0.4;
+  }
+
+  .z4 {
+    opacity: 0.2;
+  }
+
+  .z5 {
+    opacity: 0.02;
+  }
+
+  .z6 {
+    opacity: 0.05;
+  }
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,27 +1,51 @@
 <template>
   <div id="header">
-    <connection-indicator v-if="isConnected != null" v-bind:is-connected="isConnected"></connection-indicator>
+    <div class="logo">
+      <logo text="QUIZ"></logo>
+    </div>
+
+    <div class="connection-indicator">
+      <connection-indicator v-if="isConnected !== null"
+                            v-bind:is-connected="isConnected">
+      </connection-indicator>
+    </div>
   </div>
 </template>
 
 <script>
-    import ConnectionIndicator from "./ConnectionIndicator";
+  import ConnectionIndicator from "./ConnectionIndicator";
+  import Logo from "../Logo";
 
-    export default {
-        components: {
-          ConnectionIndicator
-        },
-        name: "app-header",
-        props: ['isConnected']
-    }
+  export default {
+    components: {
+      Logo,
+      ConnectionIndicator
+    },
+    name: "app-header",
+    props: ['isConnected']
+  }
 </script>
 
 <style scoped>
   #header {
     display: flex;
     flex-direction: row;
-    justify-content: flex-end;
     height: 35px;
     background-color: #6675d6;
+  }
+
+  .logo {
+    display: flex;
+    flex-basis: 93%;
+    padding-left: 20px;
+    height: 100%;
+  }
+
+  .connection-indicator {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-basis: 7%;
+    height: 100%;
   }
 </style>
